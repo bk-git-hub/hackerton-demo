2ab8919112ff6b1e9f3595f0be049ef8fdbdbfe2
bk-git-hub <bkprivate99@gmail.com>
2025-12-23 15:11:00 +0900

    chore: 메인 페이지 임시 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import ProjectCreatePage from './pages/ProjectCreatePage';
+import ProjectPage from './pages/ProjectPage';
 function App() {
   return (
     <div>
       <nav>
         {/* a 태그 대신 Link 컴포넌트를 사용해야 페이지가 새로고침되지 않습니다. */}
-        <Link to="/">홈</Link> | <Link to="/about">소개</Link>
+        <Link to="/">홈</Link> | <Link to="/projects/new">프로젝트 생성</Link> |{' '}
+        <Link to="/projects/123">프로젝트 샘플</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/projects/new" element={<ProjectCreatePage />} />
+        <Route path="/projects/:slug" element={<ProjectPage />} />
         {/* 404 페이지 설정 */}
         <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
       </Routes>
