d2d555e45354e626d2d448ff7699fa2f8e4cf045
bk-git-hub <bkprivate99@gmail.com>
2025-12-23 11:52:11 +0900

    feat: 리액트 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
-
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        {/* a 태그 대신 Link 컴포넌트를 사용해야 페이지가 새로고침되지 않습니다. */}
+        <Link to="/">홈</Link> | <Link to="/about">소개</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* 404 페이지 설정 */}
+        <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
